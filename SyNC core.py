77aac949c153b0cdd59a7103036bd9956397df28
CollabSyNC <80779893+CollabSyNC@users.noreply.github.com>
2021-03-16 22:46:57 +0530

    Add files via upload

--- SyNC core.py
+++ SyNC core.py
@@ -17,7 +17,7 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
-    b, a = butter(order, [low, high], btype='band')
+    b, a = butter(order, [low, high], btype='band',analog=False)
     return b, a
 
 
@@ -25,40 +25,6 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
-
-f1000 = open("temp1000.txt", "r")
-
-Isyn1000s = []
-RM1000s = []
-w1000s = []
-A1000s = []
-D1000s = []
-Z1000s = []
-RMtr1000s = []
-Prel1000s = []
-Pinh1000s = []
-Inh1000s = []
-Y1000s = []
-t1000s = []
-
-for line in f1000:
-    l = [elt.strip() for elt in line.split(' ')]
-    #ks.append(float(l[1]))
-    Isyn1000s.append(float(l[2]))
-    #RM.append(float(l[4]))
-    #C.append(float(l[5]))
-    w1000s.append(float(l[3]))
-    A1000s.append(float(l[4]))
-    D1000s.append(float(l[5]))
-    RMtr1000s.append(float(l[6]))
-    #Pinhs.append(float(l[1]))
-    Prel1000s.append(float(l[7]))
-    Inh1000s.append(float(l[8]))
-    #Sm.append(float(l[13]))
-    Y1000s.append(float(l[0]))
-    Z1000s.append(float(l[1]))
-    t1000s.append((float(l[9])/10000000)+3)
-#End of extraction of data from hardware file
 
 f100 = open("temp100.txt", "r")
 
@@ -94,108 +60,7 @@ for line in f100:
     t100s.append((float(l[8])/10000000)+4)
 #End of extraction of data from hardware file
 print(t100s)
-# csv file name 
-filename = "result_decimal.csv"
 
-# initializing the titles and rows list 
-fields = [] 
-rows = [] 
-
-tpos = []
-Prelpos = []
-Xpos = []
-Ypos = []
-Isynpos = []
-wpos = []
-Apos = []
-Dpos = []
-RMtrpos = []
-Inhpos = []
-
-# reading csv file 
-with open(filename, 'r') as csvfile: 
-	# creating a csv reader object 
-	csvreader = csv.reader(csvfile) 
-	
-	# extracting field names through first row 
-	fields = next(csvreader) 
-
-	# extracting each data row one by one 
-	for row in csvreader: 
-		rows.append(row) 
-
-	# get total number of rows 
-	print("Total no. of rows: %d"%(csvreader.line_num)) 
-
-# printing the field names 
-print('Field names are:' + ', '.join(field for field in fields)) 
-
-for row in rows: 
-	# parsing each column of a row 
-    if len(row)==0:
-        continue
-    tpos.append((float(row[0])/10000000000))
-    Xpos.append(float(row[3]))
-    Ypos.append(float(row[4]))
-    Isynpos.append(float(row[5]))
-    wpos.append(float(row[6]))
-    Apos.append(float(row[7]))
-    Dpos.append(float(row[8]))
-    RMtrpos.append(float(row[9]))
-    Prelpos.append(float(row[10]))
-    Inhpos.append(float(row[11]))
-    
-# csv file name 
-filename64 = "result.csv"
-
-# initializing the titles and rows list 
-fields = [] 
-rows = [] 
-
-t64 = []
-Prel64 = []
-X64 = []
-Y64 = []
-Isyn64 = []
-w64 = []
-A64 = []
-D64 = []
-RMtr64 = []
-Inh64 = []
-
-# reading csv file 
-with open(filename64, 'r') as csvfile: 
-	# creating a csv reader object 
-	csvreader = csv.reader(csvfile) 
-	
-	# extracting field names through first row 
-	fields = next(csvreader) 
-
-	# extracting each data row one by one 
-	for row in csvreader: 
-		rows.append(row) 
-
-	# get total number of rows 
-	print("Total no. of rows: %d"%(csvreader.line_num)) 
-
-# printing the field names 
-print('Field names are:' + ', '.join(field for field in fields)) 
-
-for row in rows: 
-	# parsing each column of a row 
-    if len(row)==0:
-        continue
-    t64.append(((float(row[9])/10000000)))
-    X64.append(float(row[0]))
-    Y64.append(float(row[1]))
-    Isyn64.append(float(row[2]))
-    w64.append(float(row[3]))
-    A64.append(float(row[4]))
-    D64.append(float(row[5]))
-    RMtr64.append(float(row[6]))
-    Prel64.append(float(row[7]))
-    Inh64.append(float(row[8]))
-    
 zx =[0,0,0,0,0,0]
 tt = [0]
 def I(t,tt):
@@ -203,7 +68,7 @@ def I(t,tt):
     for tx in tt:
         x += ((t-tx)/0.1)*np.exp((-t+tx)/0.1)
     return x
-def model(z,t,u,Isyn1,noise):
+def model(z,t,u,Isyn1):
     #values
     RM = 0.691
     #variables 
@@ -242,6 +107,7 @@ def model(z,t,u,Isyn1,noise):
     Isyn = w*Isyn1
     dSmdt = (((1+np.tanh(850*(Isyn-0.001)))*(1-Sm))-Sm/dsm)/0.1
     zx[0] = Isyn
+    zx[1] = Isyn
     P0[0] = Pinh
     P0[1] = Prel
     dzdt = [dRMtracedt,dAdt,dDdt,dInhxdt,dwdt,dSmdt]
@@ -279,14 +145,14 @@ def model3(z3,t,C):
     #equations
     x = z3[0]
     y = z3[1]
-    dxdt = x-(x*x*x)/3-y+C*1000000+0.14
+    dxdt = x-(x*x*x)/3-y+C*100000+0.14
     dydt = 0.08*(x+0.7-(0.8*y))
     dz3dt = [dxdt,dydt]
     return dz3dt
 
 # initial condition
 z0 = [0.000000001,0,0,0.000000001,0,0]
-zx0 = [0.0,0.0,0.0,0.0,0.0,0.0]
+zx0 = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 z20 = [0.0000001,0.0000000000001]
 z30 = [0.0000001,0.0000000000001]
 P0 =[0,0]
@@ -297,6 +163,18 @@ n = 40001
 # time points
 t = np.linspace(0,40,n)
 noise = np.random.normal(0,1,n)
+fs = 1000.0
+lowcut = 150.0
+highcut = 200.0
+
+noise_filtered = butter_bandpass_filter(noise, lowcut, highcut, fs, order=6)
+"""plt.plot(t[3600:4600], noise_filtered[3600:4600])
+plt.xlabel('time (seconds)')
+#plt.hlines([-a, a], 0, T, linestyles='--')
+plt.grid(True)
+plt.axis('tight')
+plt.legend(loc='upper left')
+plt.draw()"""
 
 # impulse input
 u = np.zeros(n)
@@ -311,6 +189,8 @@ RM = np.zeros(n)
 
 Sm = np.empty_like(t)
 Isyn = np.empty_like(t)
+Isynx = np.empty_like(t)
+Influx = np.empty_like(t)
 RMtrace = np.empty_like(t)
 A = np.empty_like(t)
 D = np.empty_like(t)
@@ -333,9 +213,11 @@ Sm[0] = z0[5]
 Pinh[0] = P0[0]
 Prel[0] = P0[1] 
 Isyn[0] = zx0[0] 
+Isynx[0] = zx0[0] 
 RM[0] = zx0[1]
 V[0] = zx0[0] 
 W[0] = zx0[1]
+Influx[0] = 0.0
 # record initial conditions
 
 # solve ODE
@@ -344,7 +226,7 @@ for i in range(1,n):
     tspan = [t[i-1],t[i]]
     Isyn1 = I(t[i-1],tt)
     # solve for next step
-    z = odeint(model,z0,tspan,args=(u[i],Isyn1,noise[i]))
+    z = odeint(model,z0,tspan,args=(u[i],Isyn1,))
     # store solution for plotting
     RMtrace[i] = z[1][0]
     A[i] = z[1][1]
@@ -368,13 +250,14 @@ for i in range(1,n):
     # next initial condition
     z0 = z[1]
 
+
 #plt.plot(t,Inhx,'b-',label='Inh')
 plt.figure(figsize=(8.3,5.8))
 #plt.plot(t[3600:4600],noise[3600:4600],'r-',label='Noise')
 #ylmt = [1.02,1.02]
 #xrng = [3.6,4.6]
 #plt.plot(xrng,ylmt,'b--',label='Limit')
-plt.plot(t[1000:2000],Isyn[1000:2000],'m-',label='Distortion')
+plt.plot(t[1600:4600],V[1600:4600],'m-',label='Distortion')
 plt.yticks(fontsize=20)
 plt.xticks(fontsize=20)
 plt.ylabel('Distortion Ratio', fontsize=24)
@@ -384,7 +267,7 @@ plt.draw()  # Draws, but does not block
 
 plt.figure(figsize=(8.3,5.8))
 #plt.plot(t[3600:4600],noise[3600:4600],'r-',label='Noise')
-plt.plot(t[1000:2000],V[1000:2000],'r-',label='Distortion')
+plt.plot(t[1600:4600],W[1600:4600],'r-',label='Distortion')
 plt.yticks(fontsize=20)
 plt.xticks(fontsize=20)
 plt.ylabel('Noise', fontsize=24)
